1c0e49062e9f0c5092b92879355db399715f067a
Emily Miura Stempel <emiura@uw.edu>
2024-03-29 00:49:44 -0700

    updated to remove plt

--- my_app.py
+++ my_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import altair as alt
 from datetime import datetime, timedelta
 from lmfit import CompositeModel, Model
